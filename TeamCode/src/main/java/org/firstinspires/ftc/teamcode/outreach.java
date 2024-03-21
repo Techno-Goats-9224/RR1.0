e2d7757128f14cade99253bbc2e74223dedb0472
Techno-Goats-9224 <ftcgoats9224@gmail.com>
2024-03-20 19:24:57 -0500

    added code for outreach bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/outreach.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * This file contains an example of an iterative (Non-Linear) "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all iterative OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp
+@Disabled
+public class outreach extends OpMode
+{private DcMotor BL;
+    private DcMotor BR;
+    private DcMotor FL;
+    private DcMotor FR;
+    private CRServo Flag;
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        Flag = hardwareMap.get(CRServo.class,"Flag");
+        BL = hardwareMap.get(DcMotor.class,"BL");
+        BR = hardwareMap.get(DcMotor.class,"BR");
+        FL = hardwareMap.get(DcMotor.class,"FL");
+        FR = hardwareMap.get(DcMotor.class,"FR");
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        double x=gamepad1.left_stick_x;
+        double y=-gamepad1.left_stick_y;
+        telemetry.addData("x",x);
+        telemetry.addData("y",y);
+
+        double ly = gamepad1.left_stick_y * 0.8;
+        double lx = -gamepad1.left_stick_x * 0.8;
+        double rx = -gamepad1.right_stick_x * 0.8;
+
+        FL.setPower(ly + lx + rx);
+        BL.setPower(ly - lx + rx);
+        FR.setPower(-ly + lx + rx);
+        BR.setPower(ly + lx - rx);
+        boolean A=gamepad1.a;
+        if (A){
+            Flag.setPower(1);
+        }else{
+            Flag.setPower(0);
+        }
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+}
+
+
